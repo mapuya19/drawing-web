0d0ae5e2cbb208fe9fea62026ea44903e51a2548
Matthew Apuya <m.apuya19@gmail.com>
2022-05-04 13:02:00 -0400

    pixelate phone images

--- final/js/phone.js
+++ final/js/phone.js
@@ -1,3 +1,4 @@
+// volume slider
 $("#volume").slider({
   min: 0,
   max: 100,
@@ -26,6 +27,7 @@ function setVolume(myVolume) {
   myMedia.volume = myVolume;
 }
 
+// sliding windows
 $(function () {
   var windowWidth = $(".window").width();
   var windowHeight = $(".window").height();
@@ -75,3 +77,29 @@ $("#volume").slider({
     setVolume(ui.value / 100);
   },
 });
+
+// pixelate images
+const filter = document.querySelector("#pixelate"),
+  dot = filter.querySelector("#dot"),
+  spacing = filter.querySelector("#spacing"),
+  size = filter.querySelector("#size");
+
+const filter2 = document.querySelector("#pixelate2"),
+  dot2 = filter.querySelector("#dot2"),
+  spacing2 = filter.querySelector("#spacing2"),
+  size2 = filter.querySelector("#size2");
+
+const filter3 = document.querySelector("#pixelate3"),
+  dot3 = filter.querySelector("#dot3"),
+  spacing3 = filter.querySelector("#spacing3"),
+  size3 = filter.querySelector("#size3");
+
+const update = function () {
+  const diameter = 5,
+    radius = diameter / 2;
+  spacing.setAttribute("width", diameter);
+  spacing.setAttribute("height", diameter);
+  size.setAttribute("radius", radius);
+};
+
+update();
